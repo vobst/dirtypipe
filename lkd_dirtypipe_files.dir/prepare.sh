4457006cbe477e49cb74dac662b0f6ed0b23ad6a
kali-vm-main <foo@bar.baz>
2022-05-23 10:17:24 +0200

    Use sudo to run commands as user

--- lkd_dirtypipe_files.dir/prepare.sh
+++ lkd_dirtypipe_files.dir/prepare.sh
@@ -4,6 +4,6 @@ useradd user && \
 cp Makefile /home/user && chown user:user /home/user/Makefile && \
 cp poc.c /home/user && chown user:user /home/user/poc.c && \
 echo "File owned by root!" > /home/user/target_file && \
-cd /home/user && su user && make all && chmod 777 poc || exit 1
-
-exit 0
+cd /home/user && \
+sudo -u user "make all && chmod 777 poc" && \
+su user || exit 1
