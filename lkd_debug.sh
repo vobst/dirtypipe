2b80f0c831b473eb4ed555a59dc154a8f7510b7f
kali-vm-main <foo@bar.baz>
2022-05-30 20:11:27 +0200

    Using variables for commit and container

--- lkd_debug.sh
+++ lkd_debug.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+PROJECT=$(basename $(pwd))
+
 docker run -it \
     --rm --cap-add=SYS_PTRACE \
     --security-opt seccomp=unconfined \
@@ -7,5 +9,5 @@ docker run -it \
     -v $(pwd)/lkd_gdbinit:/home/dbg/.gdbinit:Z \
     --net host \
     --hostname "lkd-pwndbg-container" \
-    --name lkd-pwndbg-container \
-    lkd-dirtypipe
+    --name lkd-$PROJECT-container \
+    lkd-$PROJECT
--- lkd_init.sh
+++ lkd_init.sh
@@ -2,18 +2,20 @@
 
 PATH_SSH_CONF=/home/kali/.ssh/config
 PATH_SSH_KEY=/home/kali/.ssh/id_rsa
+PROJECT=$(basename $(pwd))
+COMMIT=e783362eb54cd99b2cac8b3a9aeac942e6f6ac07
 
 # make sure we have it later so nothing times out
 sudo true || exit 1
 
 # build image for debugger container
-docker build -f lkd_Dockerfile -t lkd-dirtypipe . || exit 1
+docker build -f lkd_Dockerfile -t lkd-$PROJECT . || exit 1
 
-# get sources for an in-tree build of unpatched kernel
-wget https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/snapshot/linux-e783362eb54cd99b2cac8b3a9aeac942e6f6ac07.tar.gz && \
-tar xf linux-e783362eb54cd99b2cac8b3a9aeac942e6f6ac07.tar.gz && \
-rsync -a linux-e783362eb54cd99b2cac8b3a9aeac942e6f6ac07/ $(pwd)/  && \
-rm -rf linux-e783362eb54cd99b2cac8b3a9aeac942e6f6ac07* || exit 1
+# get sources for an in-tree build of kernel
+wget https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/snapshot/linux-$COMMIT.tar.gz && \
+tar xf linux-$COMMIT.tar.gz && \
+rsync -a linux-$COMMIT/ $(pwd)/  && \
+rm -rf linux-$COMMIT* || exit 1
 
 # configure & build kernel, create debian image
 ./lkd_build_kernel.sh && \
@@ -34,6 +36,6 @@ echo "lkd_qemu_image.qcow2" >> .dockerignore || exit 1
 
 # create gitignore
 cp .dockerignore .gitignore && \
-echo -e ".dockerignore\nlkd_vm.log" >> .gitignore || exit 1
+echo -e ".dockerignore\nlkd_vm.log\nfs/\nmm/" >> .gitignore || exit 1
 
 exit 0
